78253e1948e0de3662a9e053b1e38eaebd990fd9
Martina Quevedo <quevedomartina1705@gmail.com>
2025-05-15 21:43:14 -0300

    Update Juego.java

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -15,6 +15,9 @@ public class Juego extends InterfaceJuego
 	private Entorno entorno;
 	private Gondolf Gondolf;
 	private Image fondo;
+	private Enemigo[] enemigos = new Enemigo[50];
+	private int enemigosVivos = 0;
+	private int totalCreados = 0;
 	
 	// Variables y métodos propios de cada grupo
 	// ...
@@ -41,7 +44,8 @@ public class Juego extends InterfaceJuego
 	 * (ver el enunciado del TP para mayor detalle).
 	 */
 	public void tick()
-	{
+	{	entorno.dibujarImagen(fondo, entorno.ancho() / 2, entorno.alto() / 2, 0);
+		this.dibujarObjetos();
 		// Procesamiento de un instante de tiempo
 		// ...
 		if(entorno.estaPresionada(entorno.TECLA_DERECHA) &&  !Gondolf.colisionaPorDerecha(entorno)) {
@@ -56,15 +60,48 @@ public class Juego extends InterfaceJuego
 		if(entorno.estaPresionada(entorno.TECLA_ABAJO)&& !Gondolf.colisionaPorAbajo(entorno)) {
 			Gondolf.MoverAbajo();
 			}
-			
-		entorno.dibujarImagen(fondo, entorno.ancho() / 2, entorno.alto() / 2, 0);
-		this.dibujarObjetos();
-		}	
-	
+		if (enemigosVivos < 10 && totalCreados < 50) {
+		    enemigos[totalCreados] = generarMurcielagoAleatorio(); // genera uno nuevo
+		    enemigosVivos++;
+		    totalCreados++;
+		}
+		 // Mover, dibujar y colisionar murciélagos
+        for (int i = 0; i < enemigos.length; i++) {
+            if (enemigos[i] != null) {
+                enemigos[i].moverHacia(Gondolf.getX(), Gondolf.getY());
+                //enemigos[i].dibujar(entorno);
+
+                if (enemigos[i].colisionaCon(Gondolf.getX(), Gondolf.getY(), 20)) {
+                    enemigos[i] = null;
+                    enemigosVivos--;
+                   // Gondolf.restarVida(10); // método que vos deberías tener en Gondolf
+                }
+            }
+	  }   
+	} 	
 	public void dibujarObjetos() {
-		this.Gondolf.dibujar(entorno);
+	this.Gondolf.dibujar(entorno);
+	for (int i = 0; i < enemigos.length; i++) {
+		        if (enemigos[i] != null) {
+		            enemigos[i].dibujar(entorno);
+		        }
+		    }
+		}
 	}
-	
+	private Enemigo generarMurcielagoAleatorio() {
+	    int lado = (int)(Math.random() * 4); // 0=arriba, 1=derecha, 2=abajo, 3=izquierda
+	    int x = 0;
+	    int y = 0;
+
+	    switch (lado) {
+	        case 0: x = (int)(Math.random() * 625); y = -20; break;       // arriba
+	        case 1: x = 620; y = (int)(Math.random() * 600); break;       // derecha
+	        case 2: x = (int)(Math.random() * 625); y = 620; break;       // abajo
+	        case 3: x = -20; y = (int)(Math.random() * 600); break;       // izquierda
+	    }
+
+	    return new Enemigo(x, y, 20, 20);
+	}
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args)
